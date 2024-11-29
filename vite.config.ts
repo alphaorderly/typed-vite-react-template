6cbb434693b9005342b1cb77a8711418abd5d41e
우경훈 <khwoo@sangsang.farm>
2024-11-29 16:19:05 +0900

    feat: vitest 설정 분리

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
-    test: {
-        environment: 'jsdom',
-        setupFiles: ['./vitest-setup.ts'], // 1
-    },
 });
