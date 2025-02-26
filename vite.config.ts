b7cccb0b61084e57fa50f7439829ee3618902517
우경훈 <khwoo@sangsang.farm>
2025-02-26 15:34:36 +0900

    fix: shadcn implementation

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [react(), tsconfigPaths(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@consts': path.resolve(__dirname, 'src/consts'),
+            '@hooks': path.resolve(__dirname, 'src/hooks'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@services': path.resolve(__dirname, 'src/services'),
+            '@stores': path.resolve(__dirname, 'src/stores'),
+            '@utils': path.resolve(__dirname, 'src/utils'),
+        },
+    },
 });
